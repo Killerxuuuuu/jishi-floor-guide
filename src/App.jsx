318fa14fb2376bec05f36eaa40ac644ebe6e8a0c
Killerxuuuuu <1911334117@qq.com>
2025-05-27 21:00:02 +0800

    fix for repeat BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // 移除 BrowserRouter
 import FloorMap from "./components/FloorMap";
 import RoomDetails from "./pages/RoomDetails"; // 新增房间详情页面
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1 style={{ textAlign: "center", margin: "1rem" }}>
-          济事楼 4 楼导览系统
-        </h1>
-        <Routes>
-          <Route path="/" element={<FloorMap />} />
-          <Route path="/room/:roomId" element={<RoomDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1 style={{ textAlign: "center", margin: "1rem" }}>
+        济事楼 4 楼导览系统
+      </h1>
+      <Routes>
+        <Route path="/" element={<FloorMap />} />
+        <Route path="/room/:roomId" element={<RoomDetails />} />
+      </Routes>
+    </div>
   );
 }
 
