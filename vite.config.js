444e878deba571394175daeba6ec9b89bcf341d1
Killerxuuuuu <1911334117@qq.com>
2025-05-27 20:36:17 +0800

    fix the unshown of pictures

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jishi-floor-guide/',
+  base: process.env.NODE_ENV === 'production' ? '/jishi-floor-guide/' : '/',
   plugins: [react()],
 })
